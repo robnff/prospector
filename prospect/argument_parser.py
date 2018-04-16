60ce94b4473210c60e7b6a5ba9239137fac53e45
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2018-04-16 14:17:43 -0400

    more hacking on argparse treatment.

--- prospect/argument_parser.py
+++ prospect/argument_parser.py
@@ -1,11 +1,13 @@
 import argparse
 
 
-    parser.add_argument("--", type=, default=,
-                        help="")
+#parser.add_argument("--", type=, default=,
+#                    help="")
 
 
 def get_parser(fitter="dynesty"):
+    """Get a default prospector argument parser
+    """
     
     parser = argparse.ArgumentParser()
 
@@ -27,24 +29,23 @@ def get_parser(fitter="dynesty"):
 
 
     # --- Optimization ---
-    parser.add_argument("--do_powell", type=bool, default=False,
-                        help="Use powell minimization before sampling?")
+    parser.add_argument("--optimize", type=bool, default=False,
+                        help="Do an optimization before sampling.")
 
-    parser.add_argument("--do_levenberg", type=bool, default=True,
-                        help=("Use L-M minimization before sampling?  "
-                              "Ignored if 'do_powell' is True."))
+    parser.add_argument("--min_method", type=str, default="lm",
+                        help=("The scipy.optimize method to use for minimization"))
 
-    parser.add_argument("--ftol", type=float, default=0.5e-5,
-                        help="minimization parameter")
-
-    parser.add_argument("--maxfev", type=int, default=5000,
-                        help="minimization max function evaluations")
+    parser.add_argument("--min_opts", type=dict, default={},
+                        help="minimization parameters")
 
     parser.add_argument("--nmin", type=int, default=5,
                         help="Number of draws from the prior from which to start minimization")
 
 
     # --- emcee fitting ----
+    parser.add_argument("--emcee", type=bool, default=False,
+                        help="Do ensemble MCMC sampling with emcee.")
+    
     parser.add_argument("--nwalkers", type=int, default=64,
                         help="Number of `emcee` walkers.")
 
@@ -63,6 +64,9 @@ def get_parser(fitter="dynesty"):
                               "disk.  Useful in case the run dies.")
 
     # --- dynesty parameters ---
+    parser.add_argument("--dynesty", type=bool, default=True,
+                        help="Do nested sampling with dynesty.")
+
     parser.add_argument("--nested_bound", type=str, default="multi",
                         help="")
 
--- prospect/fitting/fitting.py
+++ prospect/fitting/fitting.py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy.optimize import minimize, least_squares
 
-from fitting import run_emcee_sampler, run_dynesty
+from fitting import run_emcee_sampler, run_dynesty_sampler
 from ..likelihood import lnlike_spec, lnlike_phot
 
-def lnprobfn(theta, model=None, obs=None, sps=None, noisemodel=(None, None),
+
+def lnprobfn(theta, model=None, obs=None, sps=None, noise=(None, None),
              residuals=False, nested=False, verbose=False):
     """Given a parameter vector and optionally a dictionary of observational
     ata and a model object, return the ln of the posterior. This requires that
@@ -15,7 +16,7 @@ def lnprobfn(theta, model=None, obs=None, sps=None, noisemodel=(None, None),
         Input parameter vector, ndarray of shape (ndim,)
 
     :param model:
-        bsfh.sedmodel model object, with attributes including ``params``, a
+        SedModel model object, with attributes including ``params``, a
         dictionary of model parameters.  It must also have ``prior_product()``,
         and ``mean_model()`` methods defined.
 
@@ -32,13 +33,12 @@ def lnprobfn(theta, model=None, obs=None, sps=None, noisemodel=(None, None),
     :returns lnp:
         Ln posterior probability.
     """
-
-    # Calculate prior probability and exit if not within prior
+    # --- Calculate prior probability and exit if not within prior ---
     lnp_prior = model.prior_product(theta, nested=nested)
     if not np.isfinite(lnp_prior):
         return -np.infty
 
-    # Generate mean model
+    # --- Generate mean model ---
     try:
         t1 = time.time()
         spec, phot, x = model.mean_model(theta, obs, sps=sps)
@@ -46,28 +46,30 @@ def lnprobfn(theta, model=None, obs=None, sps=None, noisemodel=(None, None),
     except(ValueError):
         return -np.infty
 
-    # Return chi vectors for least-squares optimization
+    # --- Optionally return chi vectors for least-squares ---
     # note this does not include priors!
     if residuals:
         chispec = chi_spec(spec, obs)
         chiphot = chi_phot(phot, obs)
         return np.concatenate([chispec, chiphot])
-    
-    # Noise modeling
-    if noisemodel is not None:
-        spec_noise, phot_noise = noisemodel
-        if spec_noise is not None:
-            spec_noise.update(**model.params)
-        if phot_noise is not None:
-            phot_noise.update(**model.params)
-        vectors = {'spec': spec, 'unc': obs['unc'],
-                   'sed': model._spec, 'cal': model._speccal,
-                   'phot': phot, 'maggies_unc': obs['maggies_unc']}
 
-    # Calculate likelihoods
+    #  --- Update Noise Model ---
+    spec_noise, phot_noise = noise
+    vectors = {}  # These should probably be copies....
+    if spec_noise is not None:
+        spec_noise.update(**model.params)
+        vectors.update({'spec': spec, "unc": obs['unc']})
+        vectors.update({'sed': model._spec, 'cal': model._speccal})
+    if phot_noise is not None:
+        phot_noise.update(**model.params)
+        vectors.update({'phot': phot, 'phot_unc': obs['maggies_unc']})
+
+    # --- Calculate likelihoods ---
     t1 = time.time()
-    lnp_spec = lnlike_spec(spec, obs=obs, spec_noise=spec_noise, **vectors)
-    lnp_phot = lnlike_phot(phot, obs=obs, phot_noise=phot_noise, **vectors)
+    lnp_spec = lnlike_spec(spec, obs=obs,
+                           spec_noise=spec_noise, **vectors)
+    lnp_phot = lnlike_phot(phot, obs=obs,
+                           phot_noise=phot_noise, **vectors)
     d2 = time.time() - t1
     if verbose:
         write_log(theta, lnp_prior, lnp_spec, lnp_phot, d1, d2)
@@ -75,55 +77,62 @@ def lnprobfn(theta, model=None, obs=None, sps=None, noisemodel=(None, None),
     return lnp_prior + lnp_phot + lnp_spec
 
 
+def fit_model(obs, model, sps, noise, lnprobfn=lnprobfn, **kwargs):
+    """
+    """
 
-
-def fit_model(obs, model, sps, noise, lnprobfn=lnprobfn, **args):
-
-    if args["optimize"]:
-        mr, best, time = run_minimize(obs, model, sps, noise,
-                                      lnprobfn=lnprobfn, **min_kwargs)
+    if kwargs["optimize"]:
+        mr, tm, best = run_minimize(obs, model, sps, noise,
+                                    lnprobfn=lnprobfn, **kwargs)
         # set to the best
         model.set_parameters(mr[best].x)
+    else:
+        mr, tm, best = [], 0.0, 0
 
-    if args["emcee"]:
-        sampler = run_emcee()
+    if kwargs["emcee"]:
+        sampler, temc = run_emcee(obs, model, sps, noise,
+                                  lnprobfn=lnprobfn, **kwargs))
+    else:
+        sampler, temc = None, 0.0
 
-        
-    if args["dynesty"]:
-        result = run_dynesty()
+    if kwargs["dynesty"]:
+        nestout, tnest = run_dynesty(obs, model, sps, noise,
+                                    lnprobfn=lnprobfn, **kwargs))
+    else:
+        nestout, tnest = None, 0.0
 
 
-def run_minimize(obs, model, sps, noise, lnprobfn=None,
-                 method='lm', **min_opts):
-
+def run_minimize(obs, model, sps, noise, lnprobfn=lnprobfn,
+                 min_method='lm', min_opts={}, nmin=1, pool=None, **kwargs):
+    """Run a minimization
+    """
     initial = model.theta.copy()
     
     lsq = ['lm']
     scalar = ['powell']
-    opts = min_kwargs
-    
-    if method in lsq:
+
+    # --- Set some options based on minimization method ---
+    if min_method in lsq:
         algorithm = least_squares
         residuals = True
-        opts["x_scale"] = "jac"
-    elif method in scalar:
+        min_opts["x_scale"] = "jac"
+    elif min_method in scalar:
         algorithm = minimize
         residuals = False
 
     args = []
     loss = partial(lnprobfn, obs=obs, model=model, sps=sps,
-                   noisemodel=noise, residuals=residuals)
-
-    minimizer = minimize_wrapper(loss, algorithm, method, opts)
-    qinit = minimizer_ball(initial, size, model)
+                   noise=noise, residuals=residuals)
+    minimizer = minimize_wrapper(loss, algorithm, min_method, min_opts)
+    qinit = minimizer_ball(initial, nmin, model)
 
     if pool is not None:
-            M = self.pool.map
+            M = pool.map
     else:
             M = map
 
     t = time.time()
-    results = list(M(minimizer,  [np.array(q) for q in qinit]))
+    results = list(M(minimizer, [np.array(q) for q in qinit]))
     tm = time.time() - t
 
     if method in lsq:
@@ -132,40 +141,43 @@ def run_minimize(obs, model, sps, noise, lnprobfn=None,
     elif method in scalar:
         best = np.argmin([p.fun for p in results])
 
-    return results, best, tm
+    return results, tm, best
 
 
-def run_emcee(obs, model, sps, noise, lnprobfn=lnprobfn, hfile=None):
-
+def run_emcee(obs, model, sps, noise, lnprobfn=lnprobfn,
+              hfile=None, pool=None, **kwargs):
+    """Run emcee.  thin wrapper on run_emcee_sampler
+    """
     q = model.theta.copy()
     
     postkwargs = {"obs": obs,
                   "model": model,
                   "sps": sps,
-                  "noisemodel"; noise,
+                  "noise"; noise,
                   "nested": False,
                   }
 
-    
     t = time.time()
     out = run_emcee_sampler(lnprobfn, q, model, kwargs=postkwargs,
-                            hdf5=hfile, pool=pool,**kwargs)
+                            hdf5=hfile, pool=pool, **kwargs)
     sampler, burn_p0, burn_prob0 = out
     ts = time.time() - t
 
-    return sampler
+    return sampler, ts
 
 
-def run_dynesty(obs, model, sps, noise):
+def run_dynesty(obs, model, sps, noise, lnprobfn=lnprobfn,
+                pool=None, **kwargs):):
 
     def prior_transform(u, model=None):
         return model.prior_transform(u)
 
-    
+
+    # Need to deal with postkwargs
+
     t = time.time()
-    dynestyout = fitting.run_dynesty_sampler(lnprobfn, prior_transform, model.ndim,
-                                             pool=pool, queue_size=nprocs, 
-                                             stop_function=stopping_function,
-                                             wt_function=weight_function,
-                                             **kwargs)
+    dynestyout = run_dynesty_sampler(lnprobfn, prior_transform, model.ndim,
+                                     pool=pool,  **kwargs)
     ts = time.time() - t
+
+    return dynestyout, ts
